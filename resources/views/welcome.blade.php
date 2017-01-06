220435f72c25d70bd4391adfb39899088f24ad0e
Adi Raka Siwi <adiraka8@gmail.com>
2017-01-06 11:51:17 +0700

    xxxxxx

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,418 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PT. ANBIOTEK</title>
+	<meta name="description" content="PT. Andalas Bioteknologi Saiyo Padang">
+	<meta name="keywords" content="bioteknologi, laboratorium, anbiotek, saiyo, andalas, bioteknologi, kimia">
+
+	<link href="favicon.ico" rel="shortcut icon" />
+
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+	<!--banner-->
+	<section id="banner" class="banner">
+		<div class="bg-color">
+			<nav class="navbar navbar-default navbar-fixed-top">
+				<div class="container">
+					<div class="col-md-12">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#"><img src="img/logo3.png" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
+						</div>
+						<div class="collapse navbar-collapse navbar-right" id="myNavbar">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="#banner"><i class="fa fa-home"></i> Beranda</a></li>
+								<li class=""><a href="#service">Produk</a></li>
+								<li class=""><a href="#about">Tentang</a></li>
+								<li class=""><a href="#testimonial">Partner</a></li>
+								<li class=""><a href="#contact">Kontak</a></li>
+								<li class=""><a href="#blog">Blog</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</nav>
+			<div class="container">
+				<div id="myCarousel" class="carousel container slide carousel-fade">
+					<div class="carousel-inner">
+						<div class="active item one animated fadeIn"></div>
+						<div class="item two"></div>
+						<div class="item three"></div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="banner-info">
+						<div class="banner-logo text-center">
+							<img src="img/logo2.png" style="height: 150px;" class="img-responsive animated bounceInDown">
+						</div>
+						<div class="banner-text text-center">
+							<h1 class="white">PT. Andalas Bioteknologi Saiyo</h1>
+							<p><b>" MENUJU KEMANDIRIAN UNTUK KESEJAHTERAAN UMAT "</b></p>
+							<a href="#service" class="btn btn-appoint hidden-xs hidden-sm">MULAI</a>
+						</div>
+						{{-- <div class="overlay-detail text-center">
+						<a href="#service"><i class="fa fa-angle-down"></i></a>
+					</div> --}}
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--/ banner-->
+<!--service-->
+<section id="service" class="section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-4 col-xs-12">
+				<div class="section-title">
+					<h2 class="head-title lg-line">PT. Andalas Bioteknologi Saiyo</h2>
+					<hr class="botm-line">
+					{{-- <p class="sec-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..</p>
+					<a href="" style="color: #0cb8b6; padding-top:10px;">Know more..</a> --}}
+				</div>
+			</div>
+			<div class="col-md-9 col-sm-8 col-xs-12">
+				<div style="visibility: visible;" class="col-sm-12 more-features-box">
+					<div class="more-features-box-text">
+						<div class="more-features-box-text-icon"> <i class="fa fa-angle-right" aria-hidden="true"></i> </div>
+						<div class="more-features-box-text-description">
+							{{-- <h3>It's something important you want to know.</h3> --}}
+							<p>Bergerak dalam penyediaan bahan-bahan dan alat terkait dengan biologi molekuler, imunologi, kultur sel, diagnostik kedokteran dan untuk kepentingan pelayanan di bidang kesehatan dan penelitian, seperti penyediaankebutuhan bahan/reagen dan alat untuk pusat layanan kesehatan (RS, praktek dokter, bidan, dan STIKES).</p>
+						</div>
+					</div>
+					<div class="more-features-box-text">
+						<div class="more-features-box-text-icon"> <i class="fa fa-angle-right" aria-hidden="true"></i> </div>
+						<div class="more-features-box-text-description">
+							{{-- <h3>It's something important you want to know.</h3> --}}
+							<p>Berkomitmen untuk membantu anda, bukan hanya dengan penyediaan bahan dan alat, namun konsultasi langsung dengan konsultan kami secara gratis. Ini dapat dilakukan melalui email perusahaan.</p>
+						</div>
+					</div>
+					<div class="more-features-box-text">
+						<div class="more-features-box-text-icon"> <i class="fa fa-angle-right" aria-hidden="true"></i> </div>
+						<div class="more-features-box-text-description">
+							{{-- <h3>It's something important you want to know.</h3> --}}
+							<p>Dinamakan PT. Andalas Bioteknologi Saiyo, yang berarti perusahaan bioteknologi yang ada di Sumatera Barat. Motto perusahaan adalah, “Menuju Kemandirian untuk Kesejahteraan Umat”, yang mengandung makna perusahaan akan diupayakan menghasilkan produk lokal sendiri yang dapat dimanfaatkan oleh rakyat Indonesia pada khususnya. Hal ini diharapkan akan menaikan kebanggaan kita sebagai bangsa Indonesia dan tidak tergantung pada produk lain dari luar.</p>
+						</div>
+					</div>
+					<div class="more-features-box-text">
+						<div class="more-features-box-text-icon"> <i class="fa fa-angle-right" aria-hidden="true"></i> </div>
+						<div class="more-features-box-text-description">
+							{{-- <h3>It's something important you want to know.</h3> --}}
+							<p>Penggunaan kata kesejahteraan umat mengandung makna, perusahaan akan bekerja semaksimal mungkin menghasilkan sesuatu yang bermanfaat bagi masyarakat dan menetapkan porsi kepemilikan untuk masyarakat.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+<!--/ service-->
+<!--about-->
+<section id="about" class="section-padding">
+	<div class="bg-color-2">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-sm-12">
+					<h2 class="ser-title">Produk Kami</h2>
+					<hr class="botm-line">
+					<div class="row stylish-panel">
+						<div class="col-md-3">
+							<div>
+								<img src="img/elisa.jpg" alt="Texto Alternativo" class="img-circle img-thumbnail">
+								<h3>Elisa Kit</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div>
+								<img src="img/kimia.jpg" alt="Texto Alternativo" class="img-circle img-thumbnail">
+								<h3>Reagen Kimia</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div>
+								<img src="img/molekular.jpg" alt="Texto Alternativo" class="img-circle img-thumbnail">
+								<h3>Biologi Molekuler</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div>
+								<img src="img/plastic.jpg" alt="Texto Alternativo" class="img-circle img-thumbnail">
+								<h3>Plastic Ware</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--/ about-->
+<!--doctor team-->
+<section id="doctor-team" class="section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="ser-title">Meet Our Doctors!</h2>
+				<hr class="botm-line">
+			</div>
+			<div class="col-md-3 col-sm-3 col-xs-6">
+				<div class="thumbnail">
+					<img src="img/doctor1.jpg" alt="..." class="team-img">
+					<div class="caption">
+						<h3>Jessica Wally</h3>
+						<p>Doctor</p>
+						<ul class="list-inline">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-3 col-xs-6">
+				<div class="thumbnail">
+					<img src="img/doctor2.jpg" alt="..." class="team-img">
+					<div class="caption">
+						<h3>Iai Donas</h3>
+						<p>Doctor</p>
+						<ul class="list-inline">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-3 col-xs-6">
+				<div class="thumbnail">
+					<img src="img/doctor3.jpg" alt="..." class="team-img">
+					<div class="caption">
+						<h3>Amanda Denyl</h3>
+						<p>Doctor</p>
+						<ul class="list-inline">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-3 col-xs-6">
+				<div class="thumbnail">
+					<img src="img/doctor4.jpg" alt="..." class="team-img">
+					<div class="caption">
+						<h3>Jason Davis</h3>
+						<p>Doctor</p>
+						<ul class="list-inline">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--/ doctor team-->
+<!--testimonial-->
+<section id="testimonial" class="section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="ser-title">Partner Kami</h2>
+				<hr class="botm-line">
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div class="testi-details">
+					<!-- Paragraph -->
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+				<div class="testi-info">
+					<!-- User Image -->
+					<a href="#"><img src="img/thumb.png" alt="" class="img-responsive"></a>
+					<!-- User Name -->
+					<h3>Alex<span>Texas</span></h3>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div class="testi-details">
+					<!-- Paragraph -->
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+				<div class="testi-info">
+					<!-- User Image -->
+					<a href="#"><img src="img/thumb.png" alt="" class="img-responsive"></a>
+					<!-- User Name -->
+					<h3>Alex<span>Texas</span></h3>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div class="testi-details">
+					<!-- Paragraph -->
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+				<div class="testi-info">
+					<!-- User Image -->
+					<a href="#"><img src="img/thumb.png" alt="" class="img-responsive"></a>
+					<!-- User Name -->
+					<h3>Alex<span>Texas</span></h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--/ testimonial-->
+<!--cta 2-->
+<section id="cta-2" class="section-padding">
+	<div class="container">
+		<div class=" row">
+			<div class="col-md-2"></div>
+			<div class="text-right-md col-md-4 col-sm-4">
+				<h2 class="section-title white lg-line">« A few words<br> about us »</h2>
+			</div>
+			<div class="col-md-4 col-sm-5">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a typek
+				<p class="text-right text-primary"><i>— Medilap Healthcare</i></p>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+	</div>
+</section>
+<!--cta-->
+<!--contact-->
+<section id="contact" class="section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="ser-title">Hubungi Kami</h2>
+				<hr class="botm-line">
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<h3>Kontak Info</h3>
+				<div class="space"></div>
+				<p><i class="fa fa-map-marker fa-fw pull-left fa-2x"></i>Komplek Cendana Mata Air Thp. VIII Blok A/4 Padang - Sumatera Barat</p>
+				<div class="space"></div>
+				<p><i class="fa fa-envelope-o fa-fw pull-left fa-2x"></i>bioteknologiandalas@yahoo.co.id</p>
+				<div class="space"></div>
+				<p><i class="fa fa-phone fa-fw pull-left fa-2x"></i>+62 751 64652</p>
+			</div>
+			<div class="col-md-8 col-sm-8 marb20">
+				<div class="contact-info">
+					<h3 class="cnt-ttl">Ingin melakukan pemesanan ? Atau beberapa pertanyaan ?</h3>
+					<div class="space"></div>
+					<div id="sendmessage">Your message has been sent. Thank you!</div>
+					<div id="errormessage"></div>
+					<form action="" method="post" role="form" class="contactForm">
+						<div class="form-group">
+							<input type="text" name="name" class="form-control br-radius-zero" id="name" placeholder="Nama Lengkap" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+							<div class="validation"></div>
+						</div>
+						<div class="form-group">
+							<input type="email" class="form-control br-radius-zero" name="email" id="email" placeholder="Alamat Email" data-rule="email" data-msg="Please enter a valid email" />
+							<div class="validation"></div>
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control br-radius-zero" name="subject" id="subject" placeholder="Judul" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+							<div class="validation"></div>
+						</div>
+						<div class="form-group">
+							<textarea class="form-control br-radius-zero" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Pesan"></textarea>
+							<div class="validation"></div>
+						</div>
+
+						<div class="form-action">
+							<button type="submit" class="btn btn-form">Kirim Pesan</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--/ contact-->
+<!--footer-->
+<footer id="footer">
+	<div class="top-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-sm-4 marb20">
+					<div class="ftr-tle">
+						<h4 class="white no-padding">Tentang Kami</h4>
+					</div>
+					<div class="info-sec">
+						<p>Praesent convallis tortor et enim laoreet, vel consectetur purus latoque penatibus et dis parturient.</p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 marb20">
+					<div class="ftr-tle">
+						<h4 class="white no-padding">Quick Links</h4>
+					</div>
+					<div class="info-sec">
+						<ul class="quick-info">
+							<li><a href="index.html"><i class="fa fa-circle"></i>Home</a></li>
+							<li><a href="#service"><i class="fa fa-circle"></i>Service</a></li>
+							<li><a href="#contact"><i class="fa fa-circle"></i>Appointment</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 marb20">
+					<div class="ftr-tle">
+						<h4 class="white no-padding">Follow us</h4>
+					</div>
+					<div class="info-sec">
+						<ul class="social-icon">
+							<li class="bglight-blue"><i class="fa fa-facebook"></i></li>
+							<li class="bgred"><i class="fa fa-google-plus"></i></li>
+							<li class="bgdark-blue"><i class="fa fa-linkedin"></i></li>
+							<li class="bglight-blue"><i class="fa fa-twitter"></i></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer-line">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					2016 @ PT. Andalas Bioteknologi Saiyo
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
+<!--/ footer-->
+
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
